6503490e869873420ca66f84f797b31e7077a8a9
Gustav Larson <gustav.larson@gmail.com>
2016-11-30 20:47:21 +0100

    Add comments

--- collectd-notify.sh
+++ collectd-notify.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#
+# Collectd plugin for sending notifications to Notify Android app (https://mashlol.github.io/notify/).
+#
+# Author: Gustav Larson
+# URL: https://github.com/gustavlarson/collectd-notify
+
 IDENTIFIER=${1}
 NOTIFICATION=$(cat)
 
@@ -11,6 +17,10 @@ data () {
 TITLE="$(data Severity): $(data Host)"
 TEXT="$(data Type) $(data TypeInstance) has reached $(data CurrentValue)"
 
+
+#
+# Send notification
+#
 curl -X GET --get \
   --data-urlencode "to=${IDENTIFIER}" \
   --data-urlencode "title=${TITLE}" \
